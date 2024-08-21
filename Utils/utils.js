45e9790eabebfd505bc5603362973c00b6afc1d8
007Baabarr <muhammad.babar16@gmail.com>
2024-08-21 18:39:57 +0500

    change update

--- Utils/utils.js
+++ Utils/utils.js
@@ -23,15 +23,15 @@ import { getAuth ,
    signInWithEmailAndPassword ,
    signOut } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
-const firebaseConfig = {
-   apiKey: "AIzaSyDmcdufQ6aznORzbHPTs_CUOs7PSRPqxeg",
-   authDomain: "add-form-data.firebaseapp.com",
-   projectId: "add-form-data",
-   storageBucket: "add-form-data.appspot.com",
-   messagingSenderId: "238751724776",
-   appId: "1:238751724776:web:aed7d9f3f3a0312a3e455d",
-   measurementId: "G-HF10J1TE8E"
-   };
+   const firebaseConfig = {
+      apiKey: "AIzaSyDbAm5K3m1v6pY8PZTKlolvj2NeYmUCqkU",
+      authDomain: "my-first-project-40f4c.firebaseapp.com",
+      projectId: "my-first-project-40f4c",
+      storageBucket: "my-first-project-40f4c.appspot.com",
+      messagingSenderId: "347554215628",
+      appId: "1:347554215628:web:bfed829dd2b60131c6f469",
+      measurementId: "G-T890F6C7RD"
+    };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
